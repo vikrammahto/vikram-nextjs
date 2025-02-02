8214b4b2498b4e296972288b022edf23c6039975
vikrammahto <vikramkr.in@gmail.com>
2025-02-02 21:14:33 +0530

    Refactor layout to remove unused font import and adjust body structure

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist } from 'next/font/google';
 import './globals.css';
 import Header from './layout/Header';
 import Footer from './layout/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
   subsets: ['latin'],
 });
 
@@ -65,11 +60,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} antialiased`}>
         <Header />
-        {children}
+        <main className="font-[family-name:var(--font-geist-sans)]">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
